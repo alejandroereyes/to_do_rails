0665c86d0ae0472e71ab73049b7613d8f7705481
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-03 15:03:07 -0500

    added route for post

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   delete '/todos/:id', to: 'todo#destroy'
 
-
+  put '/todos/:id', to: 'todo#update'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
