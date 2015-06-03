d1c85d7719c66e11293e4dcfc68b21174a16162b
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-03 14:05:52 -0500

    able to return correct user via id request & send correct error message if id not found in JSON

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,6 +4,14 @@ class TodoController < ApplicationController
     render json: Todo.all, status: 200
   end # index
 
+  def show
+    begin
+      render json: Todo.find(params[:id]), status: 200
+    rescue ActiveRecord::RecordNotFound => error
+      render json: {error: error.message}, status: 404
+    end
+  end
+
   def new
     render json: Todo.new, status: 200
   end
